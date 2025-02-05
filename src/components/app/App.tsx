e42e31dd13b01fbac70ed387b7feae0b2f00607c
Clemeit <tboulan@fastmail.com>
2025-02-04 23:29:47 -0700

    block app from bots

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -6,15 +6,50 @@ import ContentPush from "./ContentPush.tsx"
 import { ErrorBoundary } from "./ErrorBoundary.tsx"
 import Notifications from "../global/Notifications.tsx"
 
+// Check if localStorage is set. If not, show enter token form
+// If so, show the app
+
+const value = localStorage.getItem("access-token")
+
+const enterTokenForm = () => {
+    return (
+        <form>
+            <input id="token-input" type="text" placeholder="Enter token" />
+            <button
+                type="submit"
+                onClick={() => {
+                    localStorage.setItem(
+                        "access-token",
+                        (
+                            document.getElementById(
+                                "token-input"
+                            ) as HTMLInputElement
+                        ).value
+                    )
+                    window.location.reload()
+                }}
+            >
+                Enter
+            </button>
+        </form>
+    )
+}
+
 function App() {
     return (
         <ErrorBoundary>
-            <Header />
-            <Outlet />
-            <ScrollRestoration />
-            <Footer />
-            <Notifications />
-            <ContentPush />
+            {value === "clemeit" ? (
+                <>
+                    <Header />
+                    <Outlet />
+                    <ScrollRestoration />
+                    <Footer />
+                    <Notifications />
+                    <ContentPush />
+                </>
+            ) : (
+                enterTokenForm()
+            )}
         </ErrorBoundary>
     )
 }
