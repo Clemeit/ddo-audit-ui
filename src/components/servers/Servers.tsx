c06ddba3b9c90652651ddd47a2cbc44dc80a0faa
Clemeit <tboulan@fastmail.com>
2025-06-23 17:02:28 -0600

    style changes to page

--- src/components/servers/Servers.tsx
+++ src/components/servers/Servers.tsx
@@ -3,7 +3,7 @@ import Page from "../global/Page.tsx"
 import { ContentCluster } from "../global/ContentCluster.tsx"
 import NavCardCluster from "../global/NavCardCluster.tsx"
 import ServerNavigationCard from "../global/ServerNavigationCard.tsx"
-import useGetLiveData from "../../hooks/useGetLiveData.ts"
+// import useGetLiveData from "../../hooks/useGetLiveData.ts"
 import { SERVER_NAMES_LOWER } from "../../constants/servers.ts"
 import { LoadingState } from "../../models/Api.ts"
 import { toSentenceCase } from "../../utils/stringUtils.ts"
@@ -12,58 +12,58 @@ import { ReactComponent as X } from "../../assets/svg/x.svg"
 import { ReactComponent as Pending } from "../../assets/svg/pending.svg"
 
 const Servers = () => {
-    const { serverInfo } = useGetLiveData()
-    // TODO: Needs to be a call to get unique character and guild counts
+    // const { serverInfo } = useGetLiveData()
+    // // TODO: Needs to be a call to get unique character and guild counts
 
-    const cardIcon = (serverName: string) => {
-        const isOnline = serverInfo.data?.[serverName]?.is_online
-        if (isOnline === true) {
-            return <Checkmark className="shrinkable-icon" />
-        } else if (isOnline === false) {
-            return <X className="shrinkable-icon" />
-        } else {
-            return <Pending className="shrinkable-icon" />
-        }
-    }
+    // const cardIcon = (serverName: string) => {
+    //     const isOnline = serverInfo.data?.[serverName]?.is_online
+    //     if (isOnline === true) {
+    //         return <Checkmark className="shrinkable-icon" />
+    //     } else if (isOnline === false) {
+    //         return <X className="shrinkable-icon" />
+    //     } else {
+    //         return <Pending className="shrinkable-icon" />
+    //     }
+    // }
 
-    const getServerSelectContent = () => {
-        if (
-            serverInfo.loadingState === LoadingState.Initial ||
-            serverInfo.loadingState === LoadingState.Loading
-        ) {
-            return SERVER_NAMES_LOWER.sort(([serverNameA], [serverNameB]) =>
-                serverNameA.localeCompare(serverNameB)
-            ).map((serverName) => (
-                <ServerNavigationCard
-                    key={serverName}
-                    destination={`/grouping/${serverName}`}
-                    title={toSentenceCase(serverName)}
-                    content="Loading data..."
-                    icon={<Pending className="shrinkable-icon" />}
-                />
-            ))
-        }
+    // const getServerSelectContent = () => {
+    //     if (
+    //         serverInfo.loadingState === LoadingState.Initial ||
+    //         serverInfo.loadingState === LoadingState.Loading
+    //     ) {
+    //         return SERVER_NAMES_LOWER.sort(([serverNameA], [serverNameB]) =>
+    //             serverNameA.localeCompare(serverNameB)
+    //         ).map((serverName) => (
+    //             <ServerNavigationCard
+    //                 key={serverName}
+    //                 destination={`/grouping/${serverName}`}
+    //                 title={toSentenceCase(serverName)}
+    //                 content="Loading data..."
+    //                 icon={<Pending className="shrinkable-icon" />}
+    //             />
+    //         ))
+    //     }
 
-        return SERVER_NAMES_LOWER.sort(([serverNameA], [serverNameB]) =>
-            serverNameA.localeCompare(serverNameB)
-        ).map((serverName) => (
-            <ServerNavigationCard
-                key={serverName}
-                destination={`/servers/${serverName}`}
-                title={toSentenceCase(serverName)}
-                content={
-                    <>
-                        <span className="orange-text">
-                            48,194 unique characters
-                        </span>
-                        <br />
-                        <span className="blue-text">8,109 unique guilds</span>
-                    </>
-                }
-                icon={cardIcon(serverName)}
-            />
-        ))
-    }
+    //     return SERVER_NAMES_LOWER.sort(([serverNameA], [serverNameB]) =>
+    //         serverNameA.localeCompare(serverNameB)
+    //     ).map((serverName) => (
+    //         <ServerNavigationCard
+    //             key={serverName}
+    //             destination={`/servers/${serverName}`}
+    //             title={toSentenceCase(serverName)}
+    //             content={
+    //                 <>
+    //                     <span className="orange-text">
+    //                         48,194 unique characters
+    //                     </span>
+    //                     <br />
+    //                     <span className="blue-text">8,109 unique guilds</span>
+    //                 </>
+    //             }
+    //             icon={cardIcon(serverName)}
+    //         />
+    //     ))
+    // }
 
     return (
         <Page
@@ -71,7 +71,7 @@ const Servers = () => {
             description="DDO's server populations, character demographics, content popularity, and long-term trends. Check time zone activity and choose which server is best for you!"
         >
             <ContentCluster title="Select a Server">
-                <NavCardCluster>{getServerSelectContent()}</NavCardCluster>
+                {/* <NavCardCluster>{getServerSelectContent()}</NavCardCluster> */}
             </ContentCluster>
         </Page>
     )
