edb895685e06f023036879306361c12b681f7a5c
Clemeit <clemeitddo@fastmail.com>
2025-07-16 23:37:46 -0600

    general improvements

--- src/components/buttons/Buttons.tsx
+++ src/components/buttons/Buttons.tsx
@@ -4,14 +4,18 @@ import { useNavigate } from "react-router-dom"
 import { DONATE_LINK, GITHUB_LINK } from "../../constants/client.ts"
 import { ReactComponent as GiftSVG } from "../../assets/svg/gift.svg"
 
-const MakeASuggestionButton = () => {
+interface CustomButtonProps {
+    type?: "primary" | "secondary" | "tertiary" | "secondary donate"
+}
+
+const MakeASuggestionButton = ({ type = "primary" }: CustomButtonProps) => {
     const navigate = useNavigate()
     const routeToFeedbackPage = () => {
         navigate("/feedback")
     }
     return (
         <Button
-            type="primary"
+            type={type}
             onClick={routeToFeedbackPage}
             style={{ width: "9rem", height: "2.8rem" }}
         >
@@ -20,13 +24,13 @@ const MakeASuggestionButton = () => {
     )
 }
 
-const GitHubButton = () => {
+const GitHubButton = ({ type = "secondary" }: CustomButtonProps) => {
     const openGitHubLink = () => {
         window.open(GITHUB_LINK, "_blank")
     }
     return (
         <Button
-            type="secondary"
+            type={type}
             onClick={openGitHubLink}
             style={{ width: "9rem", height: "2.8rem" }}
         >
@@ -35,14 +39,14 @@ const GitHubButton = () => {
     )
 }
 
-const DonateButton = () => {
+const DonateButton = ({ type = "secondary donate" }: CustomButtonProps) => {
     const openDonationLink = () => {
         window.open(DONATE_LINK, "_blank")
     }
     return (
         <Button
             icon={<GiftSVG />}
-            type="secondary donate"
+            type={type}
             onClick={openDonationLink}
             style={{ width: "9rem", height: "2.8rem" }}
         >
