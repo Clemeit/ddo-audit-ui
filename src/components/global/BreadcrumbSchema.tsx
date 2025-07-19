222751ab7fc03b53b65383b4413a7851f1824054
Clemeit <clemeitddo@fastmail.com>
2025-07-19 16:40:30 -0600

    preparing for release

---
+++ src/components/global/BreadcrumbSchema.tsx
@@ -0,0 +1,56 @@
+import React from "react"
+import { useLocation } from "react-router-dom"
+
+interface BreadcrumbItem {
+    name: string
+    url: string
+}
+
+const BreadcrumbSchema = () => {
+    const location = useLocation()
+
+    const generateBreadcrumbs = (): BreadcrumbItem[] => {
+        const pathSegments = location.pathname.split("/").filter(Boolean)
+        const breadcrumbs: BreadcrumbItem[] = [
+            { name: "Home", url: "https://www.hcnxsryjficudzazjxty.com/" },
+        ]
+
+        let currentPath = ""
+        pathSegments.forEach((segment, index) => {
+            currentPath += `/${segment}`
+            const name = segment.charAt(0).toUpperCase() + segment.slice(1)
+            breadcrumbs.push({
+                name,
+                url: `https://www.hcnxsryjficudzazjxty.com${currentPath}`,
+            })
+        })
+
+        return breadcrumbs
+    }
+
+    const breadcrumbs = generateBreadcrumbs()
+
+    if (breadcrumbs.length <= 1) return null
+
+    const structuredData = {
+        "@context": "https://schema.org",
+        "@type": "BreadcrumbList",
+        itemListElement: breadcrumbs.map((breadcrumb, index) => ({
+            "@type": "ListItem",
+            position: index + 1,
+            name: breadcrumb.name,
+            item: breadcrumb.url,
+        })),
+    }
+
+    return (
+        <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+                __html: JSON.stringify(structuredData),
+            }}
+        />
+    )
+}
+
+export default BreadcrumbSchema
