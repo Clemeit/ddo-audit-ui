8419bf664debdb0482778d43845b7ca6aecd68a6
Clemeit <clemeitddo@fastmail.com>
2025-07-25 13:14:16 -0600

    better 404 page

--- src/components/app/NotFound.tsx
+++ src/components/app/NotFound.tsx
@@ -2,14 +2,18 @@ import React from "react"
 import Page from "../global/Page.tsx"
 import { ContentCluster } from "../global/ContentCluster.tsx"
 import Link from "../global/Link.tsx"
+import { AlphaReleasePageMessage } from "../global/CommonMessages.tsx"
 
 const NotFound = () => {
     return (
         <Page title="Not Found" description="This page does not exist.">
+            <div className="alpha-release-message">
+                <AlphaReleasePageMessage />
+            </div>
             <ContentCluster title="Not Found">
                 <p>
-                    The page you are looking for does not exist. Please check
-                    the URL and try again.
+                    The page you are looking for does not exist or hasnot been
+                    implemented yet. Please check the URL and try again.
                 </p>
                 <Link to="/">Return to the homepage</Link>
             </ContentCluster>
