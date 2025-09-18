c7145c5372514548097200f51c577377a04f0c6c
Clemeit <clemeitddo@fastmail.com>
2025-09-18 16:30:30 -0600

    remove duplicate tags

--- src/components/global/Page.tsx
+++ src/components/global/Page.tsx
@@ -94,8 +94,6 @@ const Page = ({
                 {/* Open Graph tags */}
                 <meta property="og:title" content={title} />
                 <meta property="og:description" content={description} />
-                <meta property="og:site_name" content="DDO Audit" />
-                <meta property="og:type" content="website" />
                 <meta
                     property="og:url"
                     content={`https://www.ddoaudit.com${window.location.pathname}`}
@@ -103,9 +101,6 @@ const Page = ({
                 <meta property="og:image" content={absoluteLogoUrl} />
 
                 {/* Twitter Card tags */}
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:site" content="@ddoaudit" />
-                <meta name="twitter:creator" content="@ddoaudit" />
                 <meta name="twitter:title" content={title} />
                 <meta name="twitter:description" content={description} />
                 <meta name="twitter:image" content={absoluteLogoUrl} />
